522f6ca4ed4913ae418c520ae6cbda80a606d9fd
Gmacri99 <107902237+Gmacri99@users.noreply.github.com>
2024-03-14 07:58:12 -0400

    changes

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,9 +1,21 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import bg3 from '../assets/bg/image 3.png'
+import apple from '../assets/logos_apple.svg'
+import meta from '../assets/image 18.svg'
+import google from '../assets/flat-color-icons_google.svg'
 
 const Login = () => {
     const [text, setText]= useState('Sign in')
+    const [text2, setText2]= useState('Login')
+    const [text3, setText3]= useState(false)
+
+    const handleChangetext=()=>{
+        setText3( text3 ? false: true)
+        setText(text!=='Sign in' ?  'Sign in' : 'Sign up' )
+        setText2(text2!=='Login' ? 'Login'  : 'Sign up' )
+    }
+
   return (
     <div className='login'>
         <img className="bg-login" src={bg3} alt="" />
@@ -21,7 +33,19 @@ const Login = () => {
                         <label htmlFor="password">Password</label>
                         <input type="password" name='password' />
                     </div>
+                    <button>{text2}</button>
                 </form>
+                <div className="container">
+                    <div className="line"></div>
+                    <p className='text'>Or <span>{text2}</span> using</p>
+                    <div className="line"></div>
+                </div>        
+                <div className='buttons-login'>
+                    <div><button> <img src={google}alt="" /> {text2} with Google</button></div>
+                    <div><button><img src={meta}alt="" /> {text2} with Metamask</button></div>
+                    <div>  <button> <img src={apple}alt="" /> {text2} with Apple</button></div>
+                </div>
+                <p className='have-account'>{text2==='Login' ? "Don't have an account?" : "have an account?" } <span onClick={()=>handleChangetext()}> {text}</span></p>
             </div>
         </div>
     </div>
